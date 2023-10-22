d237c3f0784520a0c5b4e5eba5b6db2e28993aeb
manish kumar savita <manishsavita109@gmail.com>
2023-10-22 16:33:09 +0530

    added feedback

---
+++ feedback/feedbackService.py
@@ -0,0 +1,29 @@
+from pickle import FALSE, TRUE
+from flask import jsonify, request
+import feedback.feedbackDao as feedbackDao
+import sessionRequest.sessionRequestDao as sessionRequestDao
+
+
+def addFeedback():
+    sessionId = request.form.get('session_request_id')
+    if(isValidSessionId(sessionId=sessionId)):
+        feedback = request.form.get('feedback')
+        rating = request.form.get('rating')
+        feedbackDao.addFeedback(sessionId=sessionId,feedback=feedback,rating=rating)
+        return jsonify({
+            "message": "feedback submitted successfully",
+            "feedbacck":feedback,
+            "rating":rating
+        })
+    return jsonify({
+        "message":"feedback submission failed"
+    })
+
+def isValidSessionId(sessionId):
+    sessionRequest = sessionRequestDao.verifySessionRequestBySessionId(sessionRequestId=sessionId)
+    if(sessionRequest!=None):
+        return TRUE
+    return FALSE
+
+
+    
