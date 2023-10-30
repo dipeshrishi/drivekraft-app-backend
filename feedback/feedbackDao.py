3ad53e18cf290f3070e1fe37925f2d9f2d6e52d5
dipeshrishi <drishi_be16@thapar.edu>
2023-10-31 00:59:35 +0530

    REST DAOS

--- feedback/feedbackDao.py
+++ feedback/feedbackDao.py
@@ -2,13 +2,15 @@
 from db import connect,disconnect
 import  configuration.currentTime as currentTime
 import logging
+from flask import  g
+
 def addFeedback(sessionId,feedback,rating):
     now = currentTime.getCurrentTimeInIst()
-    connection_pool,obj = connect()
-    mycursor = obj.cursor(buffered=True)
+    mycursor = g.cursor
     sql = f"Insert into sessionFeedback(sessionId,feedback,rating,created_at,updated_at) values('{sessionId}','{feedback}','{rating}','{now}','{now}')"
     mycursor.execute(sql)
-    obj.commit()
-    disconnect(connection_pool,obj, mycursor)
+    g.db.commit()
+    # obj.commit()
+    # disconnect(connection_pool,obj, mycursor)
     logging.info(f"sessionFeedback with for session  {sessionId} created")
     return "feedback updated"
