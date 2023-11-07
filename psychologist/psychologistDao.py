ba030874d30fa1c7eaf6cda3fecb0f5b780a585a
dipeshrishi <drishi_be16@thapar.edu>
2023-11-07 20:59:52 +0530

    api/psychologists bug fix

--- psychologist/psychologistDao.py
+++ psychologist/psychologistDao.py
@@ -21,7 +21,7 @@ def getPsychologistInOrder():
 
     for data in psyData:
         psy=psychologist.psychologist(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10],
-                     int(data[11]) + int(data[28]), data[12], data[13],data[14], data[15], data[16], data[17], data[18], data[19], data[20], data[21], data[22], data[23], data[24],data[25],data[26],data[27],data[29])
+                     int(data[11]) + int(data[28]), data[12], data[13],data[14], data[15], data[16], data[17], data[18], data[19], data[20], data[21], data[22], data[23], data[24],data[25],data[26],data[27],data[28],data[29])
 
         psychologistList.append(psy.__dict__)
 
