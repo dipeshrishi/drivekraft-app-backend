379b37f6ebd47d98852982261542a57e753f622a
dipeshrishi <drishi_be16@thapar.edu>
2023-11-11 21:26:50 +0530

    adding profile pic link in session history api

--- sessionEntity/sessionEntity.py
+++ sessionEntity/sessionEntity.py
@@ -1,8 +1,9 @@
 class sessionEntity:
-  def __init__(self,id,userId, listenerName,sessionDateTime,duration):
+  def __init__(self,id,userId, listenerName,sessionDateTime,duration, profile_pic):
     self.id = id
     self.userId = userId
     self.listenerName = listenerName
     self.sessionDateTime = sessionDateTime
     self.duration = duration
+    self.profile_pic= profile_pic
 
--- sessionEntity/sessionEntityDao.py
+++ sessionEntity/sessionEntityDao.py
@@ -6,7 +6,7 @@ def sessionHistoryList(userId):
 
 
     mycursor = g.cursor
-    query = f'''select t.id,t.userId,p.name,t.created_at,round((t.seconds_chatted)/60,0)from transaction t left join psychologist p on p.user_id = t.psychologist_id  where t.userId ={userId} order by id desc'''
+    query = f'''select t.id,t.userId,p.name,t.created_at,round((t.seconds_chatted)/60,0), p.profile_image from transaction t left join psychologist p on p.user_id = t.psychologist_id  where t.userId ={userId} order by id desc'''
     mycursor.execute(query)
     sessionList = mycursor.fetchall()
 
@@ -18,7 +18,7 @@ def sessionHistoryList(userId):
         date_obj = datetime.strptime(date_str, date_format)
         reqFormat = ' %I:%M:%p %d-%b'
         reqDateFormat=date_obj.strftime(reqFormat)
-        ses=sessionEntity.sessionEntity(session[0],session[1],session[2],reqDateFormat,session[4])
+        ses=sessionEntity.sessionEntity(session[0],session[1],session[2],reqDateFormat,session[4], session[5])
         sessionHistroyData.append(ses.__dict__)
 
 
