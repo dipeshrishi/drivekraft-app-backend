77b00e7eb1fe48bb5672f4a8960d00fc34b3d0af
gavy31 <virender@trustcircle.co>
2024-01-07 22:58:35 +0530

    Added userRole table entries

--- app/Models/mysql/userRole.py
+++ app/Models/mysql/userRole.py
@@ -1,7 +1,7 @@
 from app.database import db
 
 class UserRole(db.Model):
-    __tablename__ = 'user_role'
+    __tablename__ = 'userRole'
     id = db.Column(db.Integer, primary_key=True)
     type = db.Column(db.Enum('CUSTOMER', 'PSYCHOLOGIST'), nullable=False)
     
